be90d48fba9bf9ebe8a90d9cc2daa08e2933aea3
Darja <darja.solodovnikova@lu.lv>
2017-11-19 20:37:21 +0200

    migrations supplemented

---
+++ database/migrations/2017_11_19_183524_create_discounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('code',8);
+            $table->integer('percent')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+}
