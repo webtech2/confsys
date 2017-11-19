a6da4909d9ab3928df09fc37462cba0565293ed7
Darja <darja.solodovnikova@lu.lv>
2017-11-19 20:47:13 +0200

    relationships

--- app/City.php
+++ app/City.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    public function conferences() {
+        return $this->hasMany('App\Conference');
+    }
+    
+    public function country() {
+        return $this->belongsTo('App\Country');
+    }
 }
--- app/Conference.php
+++ app/Conference.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conference extends Model
 {
-    //
+    protected $fillable = [
+        'acronym', 'name', 'rating', 'start_date', 'end_date', 'price'
+    ];
+    public function city() { 
+        return $this->belongsTo('App\City');
+    }
 }
